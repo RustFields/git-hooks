8ed8da61374f20b666e335f638dfeba0443742c6
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-04-20 10:17:13 +0200

    feat: improved script

--- install-hooks.sh
+++ install-hooks.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-for FILE in *; do
-	if [ ! -f /path/to/file ]
-	then
-		cp ${FILE} ../.git/hooks
-		chmod 744 ../.git/hooks/${FILE}
-	else
-    		echo "Hook ${FILE} already exists."
+HOOK_NAMES="commit-msg"
+GIT_HOOK_DIR=../.git/hooks
+LOCAL_HOOK_DIR=./
+
+echo "Installing git-hooks in project"
+
+for HOOK in $HOOK_NAMES; do
+    	if [ -f $LOCAL_HOOK_DIR/$HOOK ]; then
+		echo "Installing hook $HOOK"
+      		if [ ! -h $GIT_HOOK_DIR/$HOOK -a -x $GIT_HOOK_DIR/$HOOK ]; then
+			echo "Hook $HOOK already exists, skipping."
+		else
+			cp $LOCAL_HOOK_DIR/$HOOK $GIT_HOOK_DIR/$HOOK
+			chmod 744 $GIT_HOOK_DIR/$HOOK
+			echo "Hook $HOOK installed"
+		fi
 	fi
 done
-
